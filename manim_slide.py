6d59c9e6e551434e1b5d708a5b16f1421d5f4b13
Christopher T. Chubb <cchu4043@gmail.com>
2022-06-22 00:47:14 +0200

    Updates from JU2021 talk
    Merging back in some modifications made for the JU2021 talk back into manin_slides. Most of these make transitions smoother and more predictable. Changs include:
    - Added a function to manim to offset timing, which is helpful to hack around timing bugs that seem to occur with longer files, especially at low quality
    - Changes the slide backgrounds to the same charcoal colour used in manim so that loading is more seamless
    - Fiddled with the reveal.js options to make transitions more seamless
    - Added a fast mode (c) and auto mode (a) to make testing and editing presentations more straightforward
    - Put more fleshed-out example slides including some of the formatting I prefer

--- manim_slide.py
+++ manim_slide.py
@@ -7,6 +7,7 @@ config.video_dir= "./video_slides"
 config.flush_cache = True
 config.disable_caching = True
 class SlideScene(Scene):
+    offset=0.0
     breaks=[0]
     video_slides_dir="./video_slides"
     def setup(self):
@@ -14,11 +15,14 @@ class SlideScene(Scene):
         self.breaks=[0]
 
     def slide_break(self,t=0.5):
-        self.breaks+=[self.renderer.time+t/2]
+        self.breaks+=[self.renderer.time+t/2+self.offset]
         self.wait(t)
 
+    def add_to_offset(self,t):
+        self.offset+=t
+
     def save_times(self):
-        self.breaks+=[self.renderer.time]
+        self.breaks+=[self.renderer.time+self.offset]
         out=""
         dirname=os.path.dirname(self.renderer.file_writer.movie_file_path)
         for i in range(len(self.breaks)-1):
@@ -42,37 +46,4 @@ class SlideScene(Scene):
     def print_end_message(self):
         super(SlideScene, self).print_end_message()
         self.copy_files()
-
-
-class Title(SlideScene):
-    def construct(self):
-        title = Tex(r"\bfseries\textsc{Title}").scale(1.25).shift(2.5*UP)
-        arxiv = Tex(r"\bfseries\texttt{arXiv:????.?????}").scale(.75).shift(1.5*UP)
-        name = Tex("Christopher T.\ Chubb")
-        ethz=SVGMobject("ethz_logo_white.svg").scale(1/3).next_to(1.5*DOWN,LEFT,buff=2.5)
-        udes=SVGMobject("Université_de_Sherbrooke_(logo).svg").scale(1/3).next_to(1.5*DOWN,RIGHT,buff=2.5)
-
-        self.add(title,arxiv,name,ethz,udes)
-
-        self.play(*[FadeOut(x) for x in [title,arxiv,name,ethz,udes]],run_time=0.5)
-        self.wait(0.5)
-        
-class ExampleSlide(SlideScene):
-    def construct(self):
-        circle = Circle(radius=1, color=BLUE)
-        dot = Dot()
-        dot2 = dot.copy().shift(RIGHT)
-        self.add(dot)
-
-        line = Line([3, 0, 0], [5, 0, 0])
-        self.add(line)
-
-        self.play(GrowFromCenter(circle))
-        self.slide_break()
-        self.play(Transform(dot, dot2))
-        self.slide_break()
-        self.play(MoveAlongPath(dot, circle), run_time=2, rate_func=linear)
-        self.slide_break()
-        self.play(Rotating(dot, about_point=[2, 0, 0]), run_time=1.5)
-        self.wait()
 
--- talk.py
+++ talk.py
@@ -1,32 +1,151 @@
 from manim_slide import *
 
+config.background_color = "#161c20"
+
+toc=Group(
+    Tex("1.~Slide 1"),
+    Tex("2.~Slide 2"),
+    Tex("3.~Slide 3"),
+    Tex("4.~Conclusion"),
+).arrange(DOWN,aligned_edge=LEFT,buff=0.5).move_to(ORIGIN)
+
+footer=MathTex("\\texttt{christopherchubb.com/manim\_slides}").scale(1/2).to_corner(DOWN).set_opacity(.5)
+
+# 0 Done
 class Title(SlideScene):
     def construct(self):
-        title = Tex(r"\bfseries\textsc{Manim\_slides example}").scale(1.25).shift(2.5*UP)
+        title = Tex(r"\bfseries\textsc{Title}").scale(1.25).shift(2.5*UP)
         arxiv = Tex(r"\bfseries\texttt{arXiv:????.?????}").scale(.75).shift(1.5*UP)
         name = Tex("Christopher T.\ Chubb")
+        ethz=SVGMobject("ethz_logo_white.svg").scale(1/3).next_to(1.5*DOWN,LEFT,buff=2.5)
+        udes=SVGMobject("Université_de_Sherbrooke_(logo).svg").scale(1/3).next_to(1.5*DOWN,RIGHT,buff=2.5)
+        self.add(name,title,arxiv,ethz,udes,footer)
         
-        self.add(title,arxiv,name)
+        self.play(Unwrite(title),Unwrite(arxiv),Unwrite(name),Unwrite(ethz),Unwrite(udes))
+        self.wait()
+        self.play(FadeIn(toc))
+        self.slide_break()
 
-        self.play(*[FadeOut(x) for x in [title,arxiv,name]],run_time=0.5)
-        self.wait(0.5)
-        
-class ExampleSlide(SlideScene):
+        self.play(toc[0].animate.scale(1.2).set_color(YELLOW))
+        self.slide_break()
+
+        for i in range(1,len(toc)):
+           self.play(toc[i].animate.scale(1.2).set_color(YELLOW),toc[i-1].animate.scale(1/1.2).set_color(WHITE))
+           self.slide_break()
+
+        self.play(toc[-1].animate.scale(1/1.2).set_color(WHITE))
+
+class Slide1(SlideScene):
     def construct(self):
+        tocindex=1-1
+        heading = toc[tocindex].copy()
+        self.add(toc[0:tocindex],heading,toc[tocindex+1:],footer)
+        self.play(FadeOut(toc[0:tocindex]),FadeOut(toc[tocindex+1:]), heading.animate.move_to(ORIGIN).scale(1.5).to_corner(UP))
+        self.slide_break()
+
         circle = Circle(radius=1, color=BLUE)
         dot = Dot()
         dot2 = dot.copy().shift(RIGHT)
         self.add(dot)
-
         line = Line([3, 0, 0], [5, 0, 0])
         self.add(line)
-
         self.play(GrowFromCenter(circle))
-        self.slide_break()
         self.play(Transform(dot, dot2))
         self.slide_break()
+        
         self.play(MoveAlongPath(dot, circle), run_time=2, rate_func=linear)
-        self.slide_break()
         self.play(Rotating(dot, about_point=[2, 0, 0]), run_time=1.5)
-        self.wait()
+        self.slide_break()
 
+        self.play(FadeOut(circle),FadeOut(line),FadeOut(dot))
+        self.slide_break()
+        
+        self.play(FadeIn(toc[0:tocindex]),FadeIn(toc[tocindex+1:]), ReplacementTransform(heading,toc[tocindex]))
+
+
+class Slide2(SlideScene):
+    def construct(self):
+        tocindex=2-1
+        heading = toc[tocindex].copy()
+        self.add(toc[0:tocindex],heading,toc[tocindex+1:],footer)
+        self.play(FadeOut(toc[0:tocindex]),FadeOut(toc[tocindex+1:]), heading.animate.move_to(ORIGIN).scale(1.5).to_corner(UP))
+        self.slide_break()
+
+        square = Square(color=BLUE, fill_opacity=1)
+        self.play(FadeIn(square))
+        self.play(square.animate.shift(LEFT))
+        self.slide_break()
+        
+        self.play(square.animate.set_fill(ORANGE))
+        self.play(square.animate.scale(0.3))
+        self.play(square.animate.rotate(0.4))
+        self.slide_break()
+        
+        self.play(FadeOut(square))
+        self.slide_break()
+        
+        self.play(FadeIn(toc[0:tocindex]),FadeIn(toc[tocindex+1:]), ReplacementTransform(heading,toc[tocindex]))
+
+
+class Slide3(SlideScene):
+    def construct(self):
+        tocindex=3-1
+        heading = toc[tocindex].copy()
+        self.add(toc[0:tocindex],heading,toc[tocindex+1:],footer)
+        self.play(FadeOut(toc[0:tocindex]),FadeOut(toc[tocindex+1:]), heading.animate.move_to(ORIGIN).scale(1.5).to_corner(UP))
+        self.slide_break()
+
+        text=MathTex(
+            "\\frac{d}{dx}f(x)g(x)=","f(x)\\frac{d}{dx}g(x)","+",
+            "g(x)\\frac{d}{dx}f(x)"
+        )
+        self.play(Write(text))
+        self.slide_break()
+        
+        framebox1 = SurroundingRectangle(text[1], buff = .1)
+        framebox2 = SurroundingRectangle(text[3], buff = .1)
+        self.play(
+            Create(framebox1),
+        )
+        self.slide_break()
+        self.play(
+            ReplacementTransform(framebox1,framebox2),
+        )
+        self.slide_break()
+        
+        self.play(FadeOut(text),FadeOut(framebox2))
+        self.slide_break()
+        
+        self.play(FadeIn(toc[0:tocindex]),FadeIn(toc[tocindex+1:]), ReplacementTransform(heading,toc[tocindex]))
+
+
+class Conclusion(SlideScene):
+    def construct(self):
+        tocindex=-1
+        heading = toc[tocindex].copy()
+        self.add(toc[0:tocindex],heading,toc[tocindex+1:],footer)
+        self.play(FadeOut(toc[0:tocindex]),FadeOut(toc[tocindex+1:]), heading.animate.move_to(ORIGIN).scale(1.5).to_corner(UP))
+        self.slide_break()
+
+        temp = TexTemplate()
+        temp.add_to_preamble(r"\usepackage{marvosym} \usepackage{fontawesome}")
+
+        summary=Tex("Summary of ","what ","is ","going on").scale(.75).move_to([0,2,0])
+        summary[1].set_color(YELLOW)
+        summary[3].set_color(RED)
+        
+        arxiv=Tex(r"\texttt{\bfseries arXiv:~????.?????}").next_to(summary,DOWN,buff=1).scale(.8)
+        package=Tex(r"\texttt{\bfseries github:~chubbc/manim\_slides}").next_to(arxiv,DOWN,buff=.25).scale(.8)
+
+        email=Tex(r"\faEnvelope~~\texttt{me@christopherchubb.com}", tex_template=temp)
+        website=Tex(r"\faLink~~\texttt{christopherchubb.com}", tex_template=temp)
+        twitter=Tex(r"\faTwitter~~\texttt{@QuantumChubb}", tex_template=temp)
+        github=Tex(r"\faGithub~~\texttt{chubbc}", tex_template=temp)
+        socials=VGroup(github,twitter,website,email).arrange(DOWN).scale(0.75).shift(2*DOWN)
+
+        self.play(Write(summary))
+        self.slide_break()
+        self.play(Write(arxiv),Write(package))
+        self.slide_break()
+        self.play(Write(socials))
+        self.slide_break()
